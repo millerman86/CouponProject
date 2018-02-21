64b2eedf57a941a13bd3aabe508ae0292429dd3b
amren miller <amrenmiller@gmail.com>
2018-02-20 17:55:01 -0700

    Finish composition of HomePage by extending CouponDataContainer

--- src/Main Pages/HomePage/CouponDataContainer/CouponDataContainer.js
+++ src/Main Pages/HomePage/CouponDataContainer/CouponDataContainer.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import '../../../App.css';
-import blender from '../../../../public/images/blender.jpg';
-import books from '../../../../public/images/books.jpg';
-import chocolatecake from '../../../../public/images/chocolate-cake.jpg';
-import clothes from '../../../../public/images/clothes.jpg';
-import computer from '../../../../public/images/computer.jpg';
-import cupcakes  from '../../../../public/images/cupcakes.jpg';
-import food from '../../../../public/images/food.jpg';
-import headphones from '../../../../public/images/headphones.jpg';
-import lamp from '../../../../public/images/lamp.jpg';
-import recordplayer from '../../../../public/images/recordplayer.jpg';
-import scooter from '../../../../public/images/scooter.jpg';
-import soda from '../../../../public/images/soda.jpg';
-import speakers from '../../../../public/images/speakers.jpg';
-import strawberrycake8inch from '../../../../public/images/strawberry-cake-8-inch.jpg';
-import table from '../../../../public/images/table.jpg';
-import toys from '../../../../public/images/toys.jpg';
+
 
 import DataBaseEndPoint from '../../../DataBaseEndPoint';
-
-import CouponCard from '../CouponPresentationalComponents/CouponCard';
-
-import Coupons from '../CouponPresentationalComponents/Coupons';
-
-import CouponFilter from '../CouponPresentationalComponents/CouponFilter';
 
 
 const getUser = () => {
@@ -31,71 +10,6 @@ const getUser = () => {
 };
 
 
-
-
-let products = [
-    blender,
-    books,
-    chocolatecake,
-    clothes,
-    computer,
-    cupcakes,
-    food,
-    headphones,
-    lamp,
-    recordplayer,
-    scooter,
-    soda,
-    speakers,
-    strawberrycake8inch,
-    table,
-    toys, blender,
-    books,
-    chocolatecake,
-    clothes,
-    computer,
-    cupcakes,
-    food,
-    headphones,
-    lamp,
-    recordplayer,
-    scooter,
-    soda,
-    speakers,
-    strawberrycake8inch,
-    table,
-    toys, blender,
-    books,
-    chocolatecake,
-    clothes,
-    computer,
-    cupcakes,
-    food,
-    headphones,
-    lamp,
-    recordplayer,
-    scooter,
-    soda,
-    speakers,
-    strawberrycake8inch,
-    table,
-    toys, blender,
-    books,
-    chocolatecake,
-    clothes,
-    computer,
-    cupcakes,
-    food,
-    headphones,
-    lamp,
-    recordplayer,
-    scooter,
-    soda,
-    speakers,
-    strawberrycake8inch,
-    table,
-    toys,
-];
 
 
 
@@ -251,48 +165,6 @@ class CouponDataContainer extends React.Component {
             console.log('parsing failed', ex)
         })
     };
-
-
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className='row'>
-                    <div className="col-md-2">
-
-
-                        {/*ALL OF THE COMPONENTS BELOW ARE SIMPLY THE PRESENTATIONAL LAYER OF THE DATA THAT COMES IN FROM THE DATABASE*/}
-                        <CouponFilter priceFilter={this.updatePriceFilter}
-                                      companyFilter={this.updateCompanyFilter}
-                                      productFilter={this.updateProductFilter}
-                                      dealFilter={this.updateDealFilter}
-                                      forwardPress={this.forwardPress}
-                                      backwardPress={this.backwardPress}
-                                      pageBase={this.state.pageBase}
-                                      resultsPerPage={this.state.resultsPerPage} />
-                    </div>
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-md-7 coupon-wrapper--left">
-
-                                <CouponCard couponView={this.state.regularCoupons}
-                                            couponClip={this.handleCouponClip}
-                                            products={products} />
-
-                            </div>
-
-                            <div className="col-md-3 coupon-wrapper--right text-center">
-
-                                <Coupons
-                                  coupons={this.state.featuredCoupons} />
-
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
 }
 
 export default CouponDataContainer;
--- src/Main Pages/HomePage/Homepage.js
+++ src/Main Pages/HomePage/Homepage.js
@@ -2,7 +2,7 @@
  * Created by amrenmiller on 4/6/17.
  */
 
-import React, {Component} from 'react';
+import React from 'react';
 
 
 import CouponDataContainer from './CouponDataContainer/CouponDataContainer.js';
@@ -10,23 +10,138 @@ import CouponDataContainer from './CouponDataContainer/CouponDataContainer.js';
 import '../../App.css';
 
 
-class HomePage extends CouponDataContainer { // THIS EXTENDS THE COUPONDATACONTAINERCLASS, MEANING ALL METHODS AND THE RENDER FUNCTION
+import CouponCard from './CouponPresentationalComponents/CouponCard';
 
-  constructor(props) {
-    super(props);
-  }
+import Coupons from './CouponPresentationalComponents/Coupons';
 
+import CouponFilter from './CouponPresentationalComponents/CouponFilter';
+
+import blender from './../../../public/images/blender.jpg';
+import books from './../../../public/images/books.jpg';
+import chocolatecake from './../../../public/images/chocolate-cake.jpg';
+import clothes from './../../../public/images/clothes.jpg';
+import computer from './../../../public/images/computer.jpg';
+import cupcakes  from './../../../public/images/cupcakes.jpg';
+import food from './../../../public/images/food.jpg';
+import headphones from './../../../public/images/headphones.jpg';
+import lamp from './../../../public/images/lamp.jpg';
+import recordplayer from './../../../public/images/recordplayer.jpg';
+import scooter from './../../../public/images/scooter.jpg';
+import soda from './../../../public/images/soda.jpg';
+import speakers from './../../../public/images/speakers.jpg';
+import strawberrycake8inch from './../../../public/images/strawberry-cake-8-inch.jpg';
+import table from './../../../public/images/table.jpg';
+import toys from './../../../public/images/toys.jpg';
+
+
+
+let products = [
+  blender,
+  books,
+  chocolatecake,
+  clothes,
+  computer,
+  cupcakes,
+  food,
+  headphones,
+  lamp,
+  recordplayer,
+  scooter,
+  soda,
+  speakers,
+  strawberrycake8inch,
+  table,
+  toys, blender,
+  books,
+  chocolatecake,
+  clothes,
+  computer,
+  cupcakes,
+  food,
+  headphones,
+  lamp,
+  recordplayer,
+  scooter,
+  soda,
+  speakers,
+  strawberrycake8inch,
+  table,
+  toys, blender,
+  books,
+  chocolatecake,
+  clothes,
+  computer,
+  cupcakes,
+  food,
+  headphones,
+  lamp,
+  recordplayer,
+  scooter,
+  soda,
+  speakers,
+  strawberrycake8inch,
+  table,
+  toys, blender,
+  books,
+  chocolatecake,
+  clothes,
+  computer,
+  cupcakes,
+  food,
+  headphones,
+  lamp,
+  recordplayer,
+  scooter,
+  soda,
+  speakers,
+  strawberrycake8inch,
+  table,
+  toys,
+];
+
+
+
+class HomePage extends CouponDataContainer { // THIS EXTENDS THE COUPONDATACONTAINERCLASS, MEANING ALL METHODS
 
   render() {
     return (
-      <div>
+      <div className="container-fluid">
+        <div className='row'>
+          <div className="col-md-2">
 
 
-        {super.render()}
+            {/*ALL OF THE COMPONENTS BELOW ARE SIMPLY THE PRESENTATIONAL LAYER OF THE DATA THAT COMES IN FROM THE DATABASE*/}
+            <CouponFilter priceFilter={this.updatePriceFilter}
+                          companyFilter={this.updateCompanyFilter}
+                          productFilter={this.updateProductFilter}
+                          dealFilter={this.updateDealFilter}
+                          forwardPress={this.forwardPress}
+                          backwardPress={this.backwardPress}
+                          pageBase={this.state.pageBase}
+                          resultsPerPage={this.state.resultsPerPage} />
+          </div>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-7 coupon-wrapper--left">
 
+                <CouponCard couponView={this.state.regularCoupons}
+                            couponClip={this.handleCouponClip}
+                            products={products} />
 
-        {/*THIS COMPONENT INDEPENDENTLY IMPORTS ITS OWN PRESENTATIONAL COMPONENTS AND MAKES INDEPENDENT DATABASE REQUESTS USING COMPONENTDIDMOUNT AND FETCHING*/}
+              </div>
+
+              <div className="col-md-3 coupon-wrapper--right text-center">
+
+                <Coupons
+                  coupons={this.state.featuredCoupons} />
+
+              </div>
+
+            </div>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
